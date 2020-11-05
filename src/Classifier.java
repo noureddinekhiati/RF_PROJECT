14a6d6d0f0d3b368916a3a6bb7f7ef6765448b45
khiati <nouredinekhiati14@gmail.com>
2020-11-05 17:52:27 +0100

    Initial commit

---
+++ src/Classifier.java
@@ -0,0 +1,10 @@
+public abstract class Classifier  {
+
+    public Classifier(){
+
+    }
+    public abstract void train();
+    public abstract void test();
+    public abstract float evaluate();
+
+}
