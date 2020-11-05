9bc14f75babfb36b917876189dfc53374ac4968a
khiati <nouredinekhiati14@gmail.com>
2020-11-05 17:55:36 +0100

    Initial commit

--- src/Classifier.java
+++ src/Classifier.java
@@ -1,6 +1,13 @@
 public abstract class Classifier  {
 
-    public Classifier(){
+
+    /***
+     * permet d'initialiser les paramètres du classifieur.
+     * Les classes filles étendront cette méthode en rajoutant les paramètres qui les concernent.
+     * Le paramètre obligatoire d'un classifieur est le nombre de dimensions des données qu'il aura à traiter.
+     * @param input_dimension nombre de dimensions des données qu'il aura à traiter
+     */
+    public Classifier(int input_dimension){
 
     }
     public abstract void train();
